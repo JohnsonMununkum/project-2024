9323c1b8f96542285122429bb252aa389abbaf4a
Johnson <G00419319@atu.ie>
2024-11-25 00:45:35 +0000

    about page done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="/">NavBar</Navbar.Brand>
+                <Navbar.Brand href="/">JA Plugs</Navbar.Brand>
                 <Nav className="navbar">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,15 @@
 const About = ()=>{
     return(
         <div>
-            <h1>Welcome </h1>
+            <h1>Welcome to JA Plugs!</h1>
+            <p>Your local streetwear & designer plug.<br></br>
+                Our priority & aim is to keep our clients in the freshest apparel!</p>
+            <br></br>
+            <p>We strive to elevate your fashion journey to the next level, not just offer you shoes and clothes our redesigned brand is a reflection of our dedication to supplying aa premium variety of garmeents and technology.</p>
+            <br></br>
+            <p>Our redesigned identity is accompanied by a renewed commitment to providing outstanding customer service.Our team is here to enhance your shopping experience at every turn, offering anything from personalised styling to expert advice on the latest styles.</p>
+            <br></br>
+            <p>What's the delay? HIT US A DM TODAY!</p>
         </div>
     );
 }
